650ed8993a309586bceb0dd3bd4f00795bfc3068
RenRen62 <nakayama.r@x-point-1.net>
2025-10-25 22:50:11 +0900

    feat: :sparkles: できた！コンポーネントの追加

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,15 +1,42 @@
 import { View } from 'react-native';
-import { Text } from '@/components/bases';
-import { Button } from '@/components/bases/Button';
 import { AddButton, Container } from '@/components/modules';
+import { AchievementItem } from '@/components/modules/AchievementItem';
+
+const achievementList = [
+  {
+    title:
+      '初めてのタッチ初めてのタッチ初めてのタッチ初めてのタッチ初めてのタッチ',
+    achievedAt: '12/1',
+    color: 'pink' as const
+  },
+  {
+    title: 'お箸デビュー',
+    achievedAt: '12/1',
+    color: 'yellow' as const
+  },
+  {
+    title: '初めての歯磨き',
+    achievedAt: '12/12',
+    color: 'pink' as const
+  }
+];
 
 export default function HomeScreen() {
   return (
     <>
       <Container>
-        <View className='flex-1 items-center justify-center'>
-          <Text className='text-2xl'>Home Screen</Text>
-          <Button onPress={() => null} text='次へ' />
+        <View className='mt-8 flex-1 items-center'>
+          {achievementList.map((item, index) => (
+            <AchievementItem
+              key={index}
+              title={item.title}
+              achievedAt={item.achievedAt}
+              onPress={() => console.log('Achievement pressed')}
+              hasUnderLine={achievementList.length - 1 !== index}
+              mainColor={item.color ?? 'pink'}
+              subColor={achievementList[index + 1]?.color ?? 'pink'}
+            />
+          ))}
         </View>
       </Container>
       <AddButton onPress={() => null} />
